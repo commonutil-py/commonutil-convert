b1f0e6a4f78b48137213fdeade319df71013fb25
Cheyin L <cheyinl@gmail.com>
2020-06-11 17:53:57 +0800

    bump revision

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup  # pylint: disable=import-error
 
 setup(
 		name="commonutil-convert",
-		version="3.0.3",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
+		version="3.0.4",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
 		description="Data instance convert routines",
 		packages=[
 				"commonutil_convert",
